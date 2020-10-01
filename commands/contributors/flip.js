7b4ec14623357531794c20e8ac2ba8e99cfe103a
Michael Labrecque <micklabrecque@hotmail.com>
2020-10-01 17:39:15 -0400

    modification et formating

--- commands/contributors/flip.js
+++ commands/contributors/flip.js
@@ -1,26 +1,22 @@
 exports.run = (client, message, args) => {
-    const coin_res=Math.floor(Math.random()*2);//génère un nombre random entre 0 et 1
+	//Envoie l'image gif qui flip un coin et la supprime apres 2.1 sec
+	message.channel
+		.send(message.author, {
+			files: [client.config.images_Git.flip_coinGif],
+		})
+		.then((d_msg) => d_msg.delete({ timeout: 2100 }));
 
-    //Envoie l'image gif qui flip un coin et la supprime apres 2.1 sec
-    if(client.config.debug === true)
-        message.channel.send({files: [client.config.images_Local.flip_coinGif]}).then(d_msg => d_msg.delete({timeout:2100}));
-    else
-        message.channel.send({files: [client.config.images_Git.flip_coinGif]}).then(d_msg => d_msg.delete({timeout:2100}));
-
-    //envoie l'image du resultat 2.5 seconde apres le lancement de la commande
-    setTimeout(function(){   
-        if (coin_res==0)
-        {
-            if(client.config.debug === true)
-                message.channel.send({files: [client.config.images_Local.coin_pile]});
-            else
-                message.channel.send({files: [client.config.images_Git.coin_pile]});
-        }else
-        {
-            if(client.config.debug === true)
-                message.channel.send({files: [client.config.images_Local.coin_face]});
-            else
-                message.channel.send({files: [client.config.images_Git.coin_face]});
-        }
-    }, 2500);
+	//envoie l'image du resultat 2.5 seconde apres le lancement de la commande
+	setTimeout(function () {
+		if (Math.floor(Math.random() * 2) == 0) {
+			//génère un nombre random entre 0 et 1 et vérifie quel est le résultat
+			message.channel.send(message.author, {
+				files: [client.config.images_Git.coin_pile],
+			});
+		} else {
+			message.channel.send(message.author, {
+				files: [client.config.images_Git.coin_face],
+			});
+		}
+	}, 2500);
 };
