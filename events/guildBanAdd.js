9e36ab420308c8aabbc0f78abeff941752cbf1ca
Michael Labrecque <micklabrecque@hotmail.com>
2020-10-15 15:48:01 -0400

    n'envoie pas de dm sur un log dans server-logs

--- events/guildBanAdd.js
+++ events/guildBanAdd.js
@@ -1,27 +1,27 @@
-module.exports = (client, member) => {
+module.exports = async (client, member) => {
 	const Discord = require("discord.js");
+
 	console.log(
-		`[event:guildMemberRemove.js] ${member.user.username}#${member.user.discriminator} ban from ${member.guild.name}`
+		`[event:guildBanAdd.js] ${member.user.username}#${member.user.discriminator} ban from ${member.guild.name}`
 	);
 
-	// Vérification si le serveur est celui de JDIS pour
-	// envoyer un message générique si c'est pas le cas.
-	let message;
-	if (member.guild.id === client.config.guildid) {
-		message = new Discord.MessageEmbed()
-			.setTitle(":jdis:Hello ban world!:jdis:\n")
-			.setColor("0xFF0000")
-			.setDescription(
-				"Bonjour " +
-					member.toString() +
-					", nous avons le regret de vous annoncé que dû au non respect" +
-					" de nos réglements du serveur vous avez été banni de celui-ci!"
-			)
-			.setThumbnail("https://avatars0.githubusercontent.com/u/15794364");
-	} else {
-		message = `${member.toString()}, vous avez été banni du serveur ${
-			member.guild.name
-		}`;
-	}
-	member.user.send(message).catch(console.error);
+	// On ne peux dm un user apres qu'il aie quitter un serveur
+	// Aucun message ne lui est donc envoyé et seul un log server est ajouter
+	client.channels.cache
+		.find((channel) => channel.name === "server-logs")
+		.send(
+			new Discord.MessageEmbed()
+				.setTitle("Member ban")
+				.setColor(0xffff00)
+				.setDescription("Le membre " + member.user.username + " a été banni")
+				.setAuthor(
+					member.user.username + "#" + member.user.discriminator,
+					member.user.avatarURL
+				)
+				.setFooter(member.user)
+				.setTimestamp(Date.now())
+		)
+		.catch((err2) =>
+			console.log(`[event:guildBanAdd.js] Channel server-logs non trouvé`)
+		);
 };
--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -5,20 +5,25 @@ module.exports = (client, member) => {
 		`[event:guildMemberRemove.js] ${member.user.username}#${member.user.discriminator} left ${member.guild.name}`
 	);
 
-	// Vérification si le serveur est celui de JDIS pour
-	// envoyer un message générique si c'est pas le cas.
-	let message;
-	if (member.guild.id === client.config.guildid) {
-		message = new Discord.MessageEmbed()
-			.setTitle(":jdis:Goodbye world!:jdis:\n")
-			.setColor(0xff0000)
-			.setDescription(
-				"Bonjour "+ member.toString() + ", nous sommes triste de vous voir quitter le serveur Discord de JDIS.\n" +
-					"Nous espérons que vous avez passé de merveilleux moments parmis notre communauté et espérons votre retour!\n"
-			).setFooter("Que l'apprentissage soit la force.")
-			.setThumbnail("https://avatars0.githubusercontent.com/u/15794364");
-	} else {
-		message = `${member.toString()}, vous avez quitté le serveur discord!`;
-	}
-	member.user.send(message).catch(console.error);
+	// On ne peux dm un user apres qu'il aie quitter un serveur
+	// Aucun message ne lui est donc envoyé et seul un log server est ajouter
+	client.channels.cache
+		.find((channel) => channel.name === "server-logs")
+		.send(
+			new Discord.MessageEmbed()
+				.setTitle("Member left the server")
+				.setColor(0xffff00)
+				.setDescription(
+					"Le membre " + member.user.username + " a quitter le serveur"
+				)
+				.setAuthor(
+					member.user.username + "#" + member.user.discriminator,
+					member.user.avatarURL
+				)
+				.setFooter(member.user)
+				.setTimestamp(Date.now())
+		)
+		.catch((err2) =>
+			console.log(`[event:guildMemberRemove.js] Channel server-logs non trouvé`)
+		);
 };
